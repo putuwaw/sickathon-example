64babca979d4f93ac639640b75a80c0098eef410
Putu Widyantara Artanta Wibawa <putuwaw973@gmail.com>
2023-09-23 00:06:57 +0800

    [feat] add some usefull links and tips

---
+++ pages/extra.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("Extras")
+
+'''
+These are some usefull links that you can use to learn more about Streamlit.
+- Streamlit Secrets: https://docs.streamlit.io/library/advanced-features/secrets-management
+- Streamlit Experimental Connection: https://docs.streamlit.io/library/api-reference/connections/st.experimental_connection
+- Deployment: https://docs.streamlit.io/streamlit-community-cloud/deploy-your-app
+- Connect Streamlit to Data Source: https://docs.streamlit.io/knowledge-base/tutorials/databases
+- App Dependencies: https://docs.streamlit.io/streamlit-community-cloud/deploy-your-app/app-dependencies
+
+
+You can also make visualization using others library like 
+[matplotlib](https://pypi.org/project/matplotlib/), 
+[seaborn](https://pypi.org/project/seaborn/), 
+[plotly](https://pypi.org/project/plotly/), etc.
+'''
